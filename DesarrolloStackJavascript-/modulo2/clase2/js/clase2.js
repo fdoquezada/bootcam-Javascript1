84f0258f038b8e9ef47f7d1b2a2fb9e22d4ced3b
fernandres <fdoquezadapuno@gmail.com>
2022-04-27 16:40:32 -0400

    clase 2 modulo2

---
+++ DesarrolloStackJavascript-/modulo2/clase2/js/clase2.js
@@ -0,0 +1,43 @@
+var nombreGato = "manchas";
+var NumeroTelefono = 12115145414;
+var mayorEdad = true;
+
+var  edad = prompt("ingrese su edad");
+
+if (edad>=18) {
+    alert("ud. es mayor de edad: " + edad);
+}else{
+    let edadFaltante=18-edad;
+    alert("ud. es mayor de edad , te falta "+edadFaltante+" años);
+}
+
+
+var numero1=5;
+var numero2="5";
+
+if (numero1==numero2) {
+    alert("son iaguales (1)!!!);
+
+}
+
+
+var respuesta = prompt("cuanto es 1+1 :A)3 - B)5 - C)0 - d)2");
+
+if (respuesta=="A"){
+    alert("respuesta incorresta");
+}else if(respuesta=="B"){
+    alert("respuesta incorresta");
+}else if(respuesta=="C"){
+    alert("respuesta incorresta");
+}else if (respuesta=="d"){
+    alert("respuesta Corecta !!!!!!!!!!!!!!!");
+}else {
+    alert("debe ingresar una alternativa entre A y D")
+}
+
+//desafio de las operaciones matematicas
+var num1 = prompt("ingrese un numero");
+var num2 = prompt("Ingrese otro numero");
+var num3 = prompt("Ingrese la operacion");
+
+var dia = prompt("")
