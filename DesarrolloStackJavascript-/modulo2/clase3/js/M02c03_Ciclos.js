ce0b209f1792c05baceafbcb1ff36337b235d756
fernandres <fdoquezadapuno@gmail.com>
2022-04-28 13:26:06 -0400

    fin de clase 3  modeulos2

--- DesarrolloStackJavascript-/modulo2/clase3/js/M02c03_Ciclos.js
+++ DesarrolloStackJavascript-/modulo2/clase3/js/M02c03_Ciclos.js
@@ -35,12 +35,72 @@
 //------------------------------------------------------------------------
 // ciclo para pedir numero hasta que el usuario ingrese 0   
 
-console.log("ingrese un numero");
-var numero;//numero va ser la variable donde se lea el promt
-var suma = 0;//es la variable donde ir sumando los numero ingresados  por el usuario
+// console.log("ingrese un numero");
+// var numero;//numero va ser la variable donde se lea el promt
+// var suma = 0;//es la variable donde ir sumando los numero ingresados  por el usuario
 
-while(numero!=0){
-    numero = Number(prompt("ingrese un numero(0 para salir)"));
-    suma=suma+numero;//sumamos el total
+// while(numero!=0){
+//     numero = Number(prompt("ingrese un numero(0 para salir)"));
+//     suma=suma+numero;//sumamos el total
+// }
+// alert("la suma es" +suma);
+//------------------------------------------
+// ciclo para pedir numero hasta que el usuario ingrese 0
+// var palabra="";
+// palabra = prompt("ingrese una palabra");
+// if(palabra=="hola"){
+//     alert("hola que tal");
+// }else if(palabra="chao"){
+//     alert("adios!!!");
+// }else{
+//     alert("no entiendo");
+// }
+
+// var palabra = "";
+//     while(palabra!="chao"){
+//         palabra = prompt("ingrese una palabra");
+//         if(palabra=="hola"){
+//             alert("hola que tal");
+//         }else if(palabra=="chao"){
+//             alert("adios!!!");
+//         }else{
+//             alert("no entiendo");
+//         }
+//     }
+//     alert("fin del programa");
+
+
+//------------------------------------------------------------------------
+// var nota="";
+// var contador=0;
+// var sumaNotas=0;
+
+//     while(nota!=0){
+//         nota = Number(prompt("ingrese una nota (0 para salir)"));
+//         if (nota==0){
+//             sumaNotas+=nota;
+//             contador++;
+//         }
+// }
+// var promedio = sumaNotas/contador;
+// alert("la nota final es " + promedio);
+
+
+console.log(Math.pi);
+console.log(Math.e);
+console.log(Math.random());
+console.log(Math.ceil(5.8));
+console.log(Math.floor(5.8));
+console.log(Math.round(5.8));
+
+//console.log(Math.pow)(3.3);
+
+//var myNumeroAleatorio = Math.floor(Math.random()*(X+1));
+
+var contador=0;
+while(contador<10){
+    console.log(Math.floor(Math.random()*10));
+    contador++;
 }
-alert("la suma es" +suma);
+
+
