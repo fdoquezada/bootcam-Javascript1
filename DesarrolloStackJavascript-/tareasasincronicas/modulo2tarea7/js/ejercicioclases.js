06fec460a17cdd052ce78cfd4165541cba04a0ab
fernandres <fdoquezadadpuno@gmail.com>
2022-05-04 18:43:57 -0400

    asincronico

---
+++ DesarrolloStackJavascript-/tareasasincronicas/modulo2tarea7/js/ejercicioclases.js
@@ -0,0 +1,19 @@
+//Ejercicio asincrónico 03
+// Manipular CSS
+// 1. Creá un documento HTML con un <div> dentro.
+// 2. Usando la Propiedad style del elemento en JavaScript, dale a ese <div>
+// altura, ancho y un color de fondo.
+// 3. Creá la clase .square en tu stylesheet, y dale las mismas Propiedades.
+// 4. Ahora en vez de agregársela con la Propiedad style, hacelo con la
+// Propiedad classList.
+// ¿Qué devuelve div.classList?
+// 1
+// 5. Desde tu consola, sacá la clase .square al <div>.
+// 6. Hacé una Función que cada un segundo (setInterval()) se fije si tiene la
+// clase .square. Si la tiene, que se la quite; si no la tiene, que se la agregue.
+// PISTA: Si lo hiciste con un if y un else no está mal... pero probá usando
+// classList.toggle(). Mucho más simple, ¿no?
+
+
+
+
---
+++ DesarrolloStackJavascript-/tareasasincronicas/modulo2tarea7/js/modulo_asincronica7.js
@@ -0,0 +1,17 @@
+// Ejercicio M02 C07
+// JavaScript Básico - Ejercicio asincrónico 02
+// Ejercicio: Manipular texto y contenido
+// 1. Creá un archivo HTML que, dentro de su <body> solo tenga un título <h1>.
+// 2. Por consola, cambiá el texto del título.
+// ● Para cambiar el texto del título, usá la Propiedad textContent.
+// 3. Por consola, cambiá el título por un párrafo con un texto a elección.
+// ● Para cambiar el título por un párrafo, usá la Propiedad innerHTML.
+// 4. Por consola, agregá otro párrafo al <body> con un texto a elección.
+// ● Para agregar otro párrafo al <body>, usá appendChild
+
+
+
+var elementocambiado=document.querySelectorAll("h1");
+elementocambiado.textContent="se cambio el texto por este texto";
+
+
