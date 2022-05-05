8861b2db75b10681f954c7e6011b34bb8c798285
fernandres <fdoquezadapuno@gmail.com>
2022-05-05 13:31:37 -0400

    avances de clase

--- DesarrolloStackJavascript-/modulo2/clase 8/js/M02C5_Eventos.js
+++ DesarrolloStackJavascript-/modulo2/clase 8/js/M02C5_Eventos.js
@@ -1,1 +1,15 @@
+parrafo.addEventListener("mouseover", function(){
+    parrafo.style.backgroundColor = "blue";
+    parrafo,style.color="yellow";
+});
+parrafo.addEventListener("mouseout", function(){
+    this.style.backgroundColor = "white";
+    this.style.color = "black";
+})
+
+let input = parrafo.createElement("input");
+parrafo.appendChild(input);
+input.addEventListener("keyup", function(){
+input.innerHTML = (input type="text" name="text" value="");
+
 
