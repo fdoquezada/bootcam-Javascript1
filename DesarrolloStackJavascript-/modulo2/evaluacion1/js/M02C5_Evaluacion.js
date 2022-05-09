5da0c9bc782624d4732ac9cef209db307c5fefde
fernandres <fdoquezadapuno@gmail.com>
2022-05-09 07:57:16 -0400

    evaluacion2

--- DesarrolloStackJavascript-/modulo2/evaluacion1/js/M02C5_Evaluacion.js
+++ DesarrolloStackJavascript-/modulo2/evaluacion1/js/M02C5_Evaluacion.js
@@ -8,7 +8,7 @@
 
 
 
-(function()  {
+(function actualizarTiempo()  {
         'use strict';
         var time = document.querySelector('#timeSpan');
         var i = 0;
@@ -26,14 +26,14 @@
         }, 1000);
     })();
 
-    var intervalID = window.setInterval(miFuncion, 10, 'Parametro 1', 'Parametro 2');
+//     var intervalID = window.setInterval(miFuncion, 10, 'Parametro 1', 'Parametro 2');
 
-function miFuncion(a,b) {
+// function miFuncion(a,b) {
 
-console.log(a);
-console.log(b);
-/*clearInterval(intervalID);*/
-}
+// console.log(a);
+// console.log(b);
+// /*clearInterval(intervalID);*/
+// }
 
 
     // const reload = document.getElementById('reload');
@@ -43,3 +43,93 @@ console.log(b);
     // });
 
 
+    max = 17
+    min = 0
+    const words = [
+        'californication',
+        'plataforma5',
+        'black',
+        'summer',
+        'flea',
+        'aeroplane',
+        'peppers',
+        'unlimited',
+        'arcadium',
+        'love',
+        'getaway',
+        'stadium',
+        'quixoticelixer',
+        'quarter',
+        'snow',
+        'dylan',
+        'zephyr',
+        'funky',
+        'chili'
+    ];
+    
+    function RandomWords() {
+        var palabras = Math.floor(Math.random() * (max - min));
+        console.log(palabras)
+        return words[palabras];
+    }
+    ///RandomWords()
+    
+    var palabraAleatoria = RandomWords();
+    
+    var time = 10;
+    
+    var score = 0;
+    
+    function addToDOM() {
+    
+        var palabraAleatoriaHtml = document.querySelector("h1")
+        palabraAleatoria.textContent="";
+        palabraAleatoria=RandomWords();
+        palabraAleatoriaHtml.textContent=palabraAleatoria;
+    }
+    addToDOM();
+    //funcion que aumenta el score
+    function aumentarScore(){
+        var scoreHtml=document.querySelector("#score");
+        scoreHtml.textContent=score;
+    }
+    //funcion que aumenta el tiempo
+    function mostrarTiempoAumnetado(){
+        var tiempoHtml=document.querySelector("#timeSpan");
+        tiempoHtml.textContent=time;
+    }
+    //creacion del evento par ainput
+    var cajaTexto = document.querySelector("#text");
+    cajaTexto.addEventListener("keyup",(e)=>{
+        if(e.key=="Enter"){                
+            //comparamos palabras ingresada con la del arreglo
+            if(palabraAleatoria==cajaTexto.value){
+                score++;
+                //aumentamos el tiempo
+                time=time+3
+                aumentarScore();
+                mostrarTiempoAumnetado();
+            }
+            palabraAleatoria = RandomWords();
+            addToDOM();
+            cajaTexto.value ="";
+        }
+    });
+    //funcion gameover()
+    function gameOver(){
+        //se debe completar la funcion
+        alert("El tiempo se termino");
+    }
+    //funcion para actualizar el tiempo
+    function actualizarTiempo(){
+        time--;
+        mostrarTiempoAumnetado();
+        if (time < 0) {
+            clearInterval(timeInterval);        
+            //Aqui colocar la funcion game over
+            mostrarTiempoAumnetado();
+            gameOver();
+        }
+        //fin de l afuncion
+    }
+    let timeInterval = setInterval(actualizarTiempo, 1000);
