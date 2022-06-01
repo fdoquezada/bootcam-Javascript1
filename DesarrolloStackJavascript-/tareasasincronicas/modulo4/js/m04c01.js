f0bc948a32ba772fe6b75705c3e901da54dba465
fdoquezada <fdoquezadapuno@gmail.com>
2022-06-01 14:00:47 -0400

    actualizacion

--- DesarrolloStackJavascript-/tareasasincronicas/modulo4/js/m04c01.js
+++ DesarrolloStackJavascript-/tareasasincronicas/modulo4/js/m04c01.js
@@ -40,23 +40,3 @@ $(document).ready(function(){
     $("ul").children().each(function(i,e){
     $(e).text(i);
     })
-
-    //ejemplo clonar el primer elemento de la lista y agregar al final 
-    $("ul li:first").clone().appendTo("ul");
-    //en js para crear un elemento se usa :
-    //document.createElement(p);
-    //su equivalente en jquery seria:$
-    let nuevoElement=$("<p>nuevo elemnento</p>");
-    //insertarlo en un div segundo
-    $("#segundo").append(nuevoElemento);
-    //es equivalente a 
-    $("<p>nuevo elemento 2</p>").append("#segundo");
-
-
-    //desafio 
-    //crear un tercer div (con titulo y parrafo)
-    //y agregarlo despues del segundo y antes de la lista 
-    $("<div id='tercero'><h1>Titulo3</h1><p>texto3</p></div>").insertAfter("#segundo");
-
-    -
-
