2b75c1fb72fc709d9b699768421390b248a4a4a8
Fernando Quezada <fernando@debian.fernando>
2022-06-11 22:47:11 -0400

    actualizacion de asincronico

---
+++ DesarrolloStackJavascript-/tareasasincronicas/modulo4/README.md
@@ -0,0 +1,5 @@
+# Lista-de-tareas
+Lista de tareas, usando clases y metodos.
+El usuario puede editar la tareas a realizar con un botón.
+El usuario puede eliminar la tarea realizada con un botón.
+
--- DesarrolloStackJavascript-/tareasasincronicas/modulo4/js/m04c01.js
+++
@@ -1,5 +0,0 @@
-//toda la ejecucuion del programa se inicia dentro de la funcion main()
-var input = document.querySelector(".input");
-var botonAgregar = document.querySelector(".boton-agregar");
-
-
