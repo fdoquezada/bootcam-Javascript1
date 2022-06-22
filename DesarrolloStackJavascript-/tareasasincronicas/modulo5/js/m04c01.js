e248e15b3edec2d8cdf853409dfa2657874f6f3f
fdoquezada <fdoquezadapuno@gmail.com>
2022-06-22 09:37:43 -0400

    se creo carpeta modulo6

---
+++ DesarrolloStackJavascript-/tareasasincronicas/modulo5/js/m04c01.js
@@ -0,0 +1,5 @@
+//toda la ejecucuion del programa se inicia dentro de la funcion main()
+var input = document.querySelector(".input");
+var botonAgregar = document.querySelector(".boton-agregar");
+
+
