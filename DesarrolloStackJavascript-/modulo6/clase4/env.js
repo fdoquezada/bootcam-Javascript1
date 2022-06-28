a50328921cc09f05eeef15e18b23bebd66d00e9d
fdoquezada <fdoquezadapuno@gmail.com>
2022-06-28 12:25:17 -0400

    actualizaion de clase 5 modulo 6

--- DesarrolloStackJavascript-/modulo6/clase4/env.js
+++ DesarrolloStackJavascript-/modulo6/clase4/env.js
@@ -7,9 +7,11 @@ console.log(process.env.CORREO);
 console.log(process.env["PUERTO"]);
 
 var http = require('http');
+ const puerto = process.env.PUERTO || 8081
+
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
-}).listen(process.env.PUERTO);
+  response.end('Prueva de archivos ');
+}).listen(puerto);
 
-console.log('Server running at: ' + process.env.PUERTO);
+console.log('Server running at: ' + puerto);
